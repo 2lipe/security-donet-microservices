08f0487f6eea01a17415d27757598e922aab736f
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-23 14:47:39 -0300

    chore: create TestUser

--- IndentityServer/Config.cs
+++ IndentityServer/Config.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 
@@ -19,6 +22,30 @@ namespace IndentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = { "movieApi" }
+                },
+                new Client
+                {
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5005/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5005/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
 
@@ -36,11 +63,24 @@ namespace IndentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
 
         public static List<TestUser> TestUsers =>
             new List<TestUser>
             {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "felipe",
+                    Password = "felipe",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "felipe"),
+                        new Claim(JwtClaimTypes.FamilyName, "vieira")
+                    }
+                }
             };
     }
 }
