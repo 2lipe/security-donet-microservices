5449ed7de6aebd18231fdb22f9be750228662335
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-23 12:10:27 -0300

    chore: Init Movies.Client

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            
+            movieList.Add(
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Comics",
+                    Title = "asd",
+                    Rating = "9.2",
+                    ImageUrl = "images/src",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "swn"
+                }
+                );
+
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
